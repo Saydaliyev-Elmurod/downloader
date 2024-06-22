e8a0c25b4ed31334cb0d8bff1eaeeb9c8b6b2b64
saydaliyev elmurod <saydaliyevelmurod01@gmail.com>
2024-06-22 22:12:02 +0500

    liquebase fix

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.spring.dependency.management)
     java
 }
+extra["springCloudVersion"] = "2023.0.2"
 
 dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-webflux")
@@ -10,9 +11,14 @@ dependencies {
 
     implementation ("org.springdoc:springdoc-openapi-webflux-ui:1.8.0")
 
-//    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     testImplementation ("org.springframework.boot:spring-boot-starter-test")
     testImplementation ("io.projectreactor:reactor-test")
     testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
