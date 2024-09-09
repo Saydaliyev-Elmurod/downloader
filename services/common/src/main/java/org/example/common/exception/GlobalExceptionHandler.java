e54dd7d8a7b50ef7544f5d872759fb174f4d6969
saydaliyev elmurod <saydaliyevelmurod01@gmail.com>
2024-09-09 12:10:14 +0500

    exception handler fix

--- services/common/src/main/java/org/example/common/exception/GlobalExceptionHandler.java
+++ services/common/src/main/java/org/example/common/exception/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ServerWebExchange;
 
 import java.io.IOException;
 import java.time.format.DateTimeParseException;
@@ -35,8 +35,8 @@ public class GlobalExceptionHandler {
             InvalidArgumentException.class,
             InvalidOperationException.class
     })
-    public ResponseEntity<?> handleBadRequests(final RuntimeException e, final WebRequest request) {
-        return constructExceptionResponse(e, request, BAD_REQUEST);
+    public ResponseEntity<?> handleBadRequests(final RuntimeException e, final ServerWebExchange exchange) {
+        return constructExceptionResponse(e, exchange, BAD_REQUEST);
     }
 
     @ResponseStatus(FORBIDDEN)
@@ -50,33 +50,33 @@ public class GlobalExceptionHandler {
             UserBlockedException.class
     })
     public final ResponseEntity<?> handleForbiddenException(
-            final ForbiddenException e, final WebRequest request) {
-        return constructExceptionResponse(e, request, FORBIDDEN);
+            final ForbiddenException e, final ServerWebExchange exchange) {
+        return constructExceptionResponse(e, exchange, FORBIDDEN);
     }
 
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<?> handleException(final Exception e, final WebRequest request) {
-        return constructExceptionResponse(e, request, INTERNAL_SERVER_ERROR);
+    public final ResponseEntity<?> handleException(final Exception e, final ServerWebExchange exchange) {
+        return constructExceptionResponse(e, exchange, INTERNAL_SERVER_ERROR);
     }
 
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(IOException.class)
-    public ResponseEntity<?> handleIOException(final IOException e, final WebRequest request) {
-        return constructExceptionResponse(e, request, INTERNAL_SERVER_ERROR);
+    public ResponseEntity<?> handleIOException(final IOException e, final ServerWebExchange exchange) {
+        return constructExceptionResponse(e, exchange, INTERNAL_SERVER_ERROR);
     }
 
     @ResponseStatus(UNPROCESSABLE_ENTITY)
     @ExceptionHandler({JsonParsingException.class, DecodingException.class})
-    public final ResponseEntity<?> handleJsonException(final Exception e, final WebRequest request) {
-        return constructExceptionResponse(e, request, UNPROCESSABLE_ENTITY);
+    public final ResponseEntity<?> handleJsonException(final Exception e, final ServerWebExchange exchange) {
+        return constructExceptionResponse(e, exchange, UNPROCESSABLE_ENTITY);
     }
 
     @ResponseStatus(NOT_FOUND)
     @ExceptionHandler({NotFoundException.class, NoSuchElementException.class})
     public ResponseEntity<?> handleNotFoundException(
-            final NoSuchElementException e, final WebRequest request) {
-        return constructExceptionResponse(e, request, NOT_FOUND);
+            final NoSuchElementException e, final ServerWebExchange exchange) {
+        return constructExceptionResponse(e, exchange, NOT_FOUND);
     }
 
     @ResponseStatus(UNAUTHORIZED)
@@ -86,27 +86,26 @@ public class GlobalExceptionHandler {
             UnauthorizedException.class,
     })
     public final ResponseEntity<?> handleUnauthorizedException(
-            final UnauthorizedException e, final WebRequest request) {
-        return constructExceptionResponse(e, request, UNAUTHORIZED);
+            final UnauthorizedException e, final ServerWebExchange exchange) {
+        return constructExceptionResponse(e, exchange, UNAUTHORIZED);
     }
 
     @ExceptionHandler({UnauthorizedExceptionYandex.class})
     public final ResponseEntity<?> handleUnauthorizedException2(
-            final UnauthorizedException e, final WebRequest request) {
-        return constructExceptionResponse(e, request, UNAUTHORIZED);
+            final UnauthorizedException e, final ServerWebExchange exchange) {
+        return constructExceptionResponse(e, exchange, UNAUTHORIZED);
     }
 
     @ResponseStatus(SERVICE_UNAVAILABLE)
     @ExceptionHandler(ServiceUnavailableException.class)
     public ResponseEntity<?> handleServiceUnavailableException(
-            final ServiceUnavailableException e, final WebRequest request) {
-        return constructExceptionResponse(e, request, SERVICE_UNAVAILABLE);
+            final ServiceUnavailableException e, final ServerWebExchange exchange) {
+        return constructExceptionResponse(e, exchange, SERVICE_UNAVAILABLE);
     }
 
-
     protected ResponseEntity<ExceptionResponse> constructExceptionResponse(
-            final Exception e, final WebRequest request, final HttpStatus status) {
-        final String path = request.getDescription(false);
+            final Exception e, final ServerWebExchange exchange, final HttpStatus status) {
+        final String path = exchange.getRequest().getPath().toString();
 
         LOGGER.error("Failed to request [{}] path. Error:", path, e);
         return new ResponseEntity<>(
